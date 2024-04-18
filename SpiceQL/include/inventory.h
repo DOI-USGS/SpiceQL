d4561362ba97cd925d364ea985c8fb53edad890b
Kelvin <krodriguez@usgs.gov>
2024-04-18 13:48:02 -0700

    added inv files

---
+++ SpiceQL/include/inventory.h
@@ -0,0 +1,17 @@
+#pragma once
+/**
+ * @file
+ *
+ * Functions for reading and writing Kernels
+ *
+ **/
+
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <nlohmann/json.hpp>
+
+namespace SpiceQL {
+
+}
