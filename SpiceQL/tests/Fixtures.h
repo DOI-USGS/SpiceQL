8a096e73c982108f0d6835ea6e7d134770f1089d
Kelvin <krodriguez@usgs.gov>
2025-05-15 15:18:49 -0700

    fixed tests

--- SpiceQL/tests/Fixtures.h
+++ SpiceQL/tests/Fixtures.h
@@ -13,15 +13,15 @@ using namespace std;
 using namespace SpiceQL;
 
 
-class TempTestingFiles : public ::testing::Environment {
-  protected:
+class TempTestingFiles : public ::testing::Test {
+  public:
     fs::path tempDir;
 
     void SetUp() override;
     void TearDown() override;
 };
 
-class IsisDataDirectory : public ::testing::Test {
+class IsisDataDirectory : public TempTestingFiles {
   protected: 
     
     string base;
@@ -36,9 +36,8 @@ class IsisDataDirectory : public ::testing::Test {
     void CompareKernelSets(vector<string> kVector, vector<string> expectedSubSet);
 };
 
-class KernelDataDirectories : public ::testing::Test  {
+class KernelDataDirectories : public TempTestingFiles  {
   protected:
-
     vector<string> paths;
 
     void SetUp() override;
@@ -46,7 +45,7 @@ class KernelDataDirectories : public ::testing::Test  {
 };
 
 
-class LroKernelSet : public ::testing::Test  {
+class LroKernelSet : public TempTestingFiles  {
   protected:
     fs::path root;
     string lskPath;
@@ -62,6 +61,7 @@ class LroKernelSet : public ::testing::Test  {
     string fkPath;
     string moonPckPath;
     string basePckPath;  
+    string spqldbPath;
 
     nlohmann::json conf;
 
@@ -69,7 +69,7 @@ class LroKernelSet : public ::testing::Test  {
     void TearDown() override;
 };
 
-class KernelsWithQualities : public ::testing::Test  {
+class KernelsWithQualities : public TempTestingFiles  {
   protected:
     fs::path root;
     string spkPathPredict; 
