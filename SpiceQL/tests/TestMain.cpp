41a11004f0252b038c4dcbc6fd9e4f5500aab534
kelvinrr <krodriguez@usgs.gov>
2026-06-26 19:35:14 +0000

    test fix

--- SpiceQL/tests/TestMain.cpp
+++ SpiceQL/tests/TestMain.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char **argv) {
    // Run the tests at trace level. The logger reads SPICEQL_LOG_LEVEL once on
    // first use, so set it before any test logs.
-   setenv("SPICEQL_LOG_LEVEL", "trace", 1);
+   setenv("SPICEQL_LOG_LEVEL", "debug", 1);
 
    ::testing::InitGoogleTest(&argc, argv);
 
